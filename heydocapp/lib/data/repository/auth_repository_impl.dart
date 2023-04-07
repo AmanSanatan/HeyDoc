c5028c58c1dbca2b7744e986e33ee2ab5f772ca2
AbhinavISM <111358311+AbhinavISM@users.noreply.github.com>
2023-04-08 02:13:52 +0530

    backend for signup

---
+++ heydocapp/lib/data/repository/auth_repository_impl.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import '../../domain/repository/auth_repository.dart';
+import '../../utils/auth_response.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+  @override
+  Future<AuthResponse> emailRegister(String email, String password) async {
+    try {
+      var result = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      return AuthResponse.success(result);
+    } on FirebaseAuthException catch (e) {
+      return AuthResponse.faliure(e.message ?? 'some error occured');
+    } catch (e) {
+      return const AuthResponse.faliure('some error occured');
+    }
+  }
+
+  @override
+  Future<AuthResponse> googleRegister() async {
+    try {
+      final GoogleSignInAccount? newGoogleUser = await GoogleSignIn().signIn();
+      if (newGoogleUser != null) {
+        final GoogleSignInAuthentication googleAuth =
+            await newGoogleUser.authentication;
+        final credential = GoogleAuthProvider.credential(
+          accessToken: googleAuth.accessToken,
+          idToken: googleAuth.idToken,
+        );
+        var result =
+            await FirebaseAuth.instance.signInWithCredential(credential);
+        return AuthResponse.success(result);
+      } else {
+        return const AuthResponse.faliure("please select a account");
+      }
+    } on FirebaseAuthException catch (e) {
+      return AuthResponse.faliure(e.message ?? 'some error occured');
+    } catch (e) {
+      return const AuthResponse.faliure('some error occured');
+    }
+  }
+
+  @override
+  Future logout() {
+    return FirebaseAuth.instance.signOut();
+  }
+}
