a4652f100d9b38d9d0f6c6b076e543a955f05e32
AbhinavISM <111358311+AbhinavISM@users.noreply.github.com>
2023-04-08 01:19:18 +0530

    firebase-riverpod dependency setup

--- heydocapp/lib/main.dart
+++ heydocapp/lib/main.dart
@@ -1,7 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
